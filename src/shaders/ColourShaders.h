ae83624dbf80d69a217ccea8276d98a54bcd0bae
jmacey <jmacey@bournemouth.ac.uk>
2014-10-13 18:47:00 +0100

    begining of the C++ 11 branch

--- src/shaders/ColourShaders.h
+++ src/shaders/ColourShaders.h
@@ -1,28 +1,7 @@
 #ifndef COLOURSHADERS_H__
 #define COLOURSHADERS_H__
-// see below for the really cool c++ 11 version of this
 
-static const char * colourVertexShader[]={
-  "#version 150 \n"
-  "uniform mat4 MVP; \n"
-  "in vec3 inVert; \n"
-  "uniform vec4 Colour; \n"
-  "void main(void) \n"
-  "{ \n"
-  " gl_Position = MVP*vec4(inVert, 1.0); \n"
-  "}  \n\0"
-};
 
-static const char * colourFragmentShader[]={
-  "#version 150 \n"
-  "uniform vec4 Colour; \n"
-  "out vec4 outColour;  \n"
-  "void main () \n"
-  "{ \n"
-  "  outColour = Colour; \n"
-  "}  \n\0"
-};
-/*
 const std::string colourVertexShader =
 R"DELIM(
 #version 150
@@ -67,6 +46,6 @@ void main ()
 
 
 )DELIM";
-*/
+
 #endif
 
