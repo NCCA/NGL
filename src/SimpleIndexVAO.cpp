b84c1a7823aa174dd84ed56fd8cd4bf360c376d9
jmacey <jmacey@bournemouth.ac.uk>
2016-04-08 11:57:56 +0100

    Added an indexVAO class and tested with demo

--- src/SimpleIndexVAO.cpp
+++ src/SimpleIndexVAO.cpp
@@ -64,27 +64,4 @@ namespace ngl
     m_indexType=_indexType;
   }
 
-
-
-/*
-
-  void SimpleIndexVAO::setData(size_t _size,const GLfloat &_data,GLenum _mode)
-  {
-    if(m_bound == false)
-    {
-      std::cerr<<"trying to set VOA data when unbound\n";
-    }
-    if( m_allocated ==true)
-    {
-        glDeleteBuffers(1,&m_buffer);
-    }
-
-    glGenBuffers(1, &m_buffer);
-    // now we will bind an array buffer to the first one and load the data for the verts
-    glBindBuffer(GL_ARRAY_BUFFER, m_buffer);
-    glBufferData(GL_ARRAY_BUFFER,static_cast<GLsizeiptr>( _size), &_data, _mode);
-    m_allocated=true;
-
-  }
-*/
 }
