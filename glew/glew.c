1484e46de2d9a8e7e22a25cbc923e075d9bc31ea
jmacey <jmacey@bournemouth.ac.uk>
2017-11-01 08:56:14 +0000

    updated glew source to use local headers as missing on docker image

--- glew/glew.c
+++ glew/glew.c
@@ -52,7 +52,7 @@
 #  endif
 #  include "wglew.h"
 #elif !defined(__ANDROID__) && !defined(__native_client__) && !defined(__HAIKU__) && (!defined(__APPLE__) || defined(GLEW_APPLE_GLX))
-#  include <GL/glxew.h>
+#  include "glxew.h"
 #endif
 
 #include <stddef.h>  /* For size_t */
