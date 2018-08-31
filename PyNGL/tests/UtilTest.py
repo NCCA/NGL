4e4a83cfd460b8c2a9db5d89e4a90eb57ed391fc
jmacey <jmacey@bournemouth.ac.uk>
2018-08-31 14:45:38 +0100

    updated python tests

---
+++ PyNGL/tests/UtilTest.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+import unittest
+import pyngl
+
+# Note all pyngl types use internal fuzzy float equality operators
+
+class TestUtil(unittest.TestCase):
+
+if __name__ == '__main__':
+     unittest.main()
