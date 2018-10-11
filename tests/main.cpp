f596dbd243667c7980641a1f66be99c9f567f5f8
jmacey <jmacey@bournemouth.ac.uk>
2018-10-11 08:31:13 +0100

    removed Message tests from non GL tests as they cause issues on remote platforms

--- tests/main.cpp
+++ tests/main.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv)
   if(useOpenGL == false)
   {
     std::cerr<<"excluding tests\n";
-    ::testing::GTEST_FLAG(filter) = "-ShaderLib.*:VAOPrimitives.*:NGLInit*";
+    ::testing::GTEST_FLAG(filter) = "-ShaderLib.*:VAOPrimitives.*:NGLInit*:NGLMessage*";
 
   }
   return RUN_ALL_TESTS();
