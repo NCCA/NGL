9dd8af9f39df381b343ff0152e570baa0593efe5
jmacey <jmacey@bournemouth.ac.uk>
2021-02-17 13:23:57 +0000

    NGLConfig.cmake had include for Qt in it which is not needed for SDL

--- NGLConfig.cmake
+++ NGLConfig.cmake
@@ -33,7 +33,7 @@ link_directories($ENV{HOME}/NGL/lib)
 
 # add exe and link libs that must be after the other defines
 link_libraries(OpenImageIO::OpenImageIO OpenImageIO::OpenImageIO_Util)
-link_libraries( ${PROJECT_LINK_LIBS}  Qt5::Widgets fmt::fmt-header-only freetype )
+link_libraries( ${PROJECT_LINK_LIBS}  fmt::fmt-header-only freetype )
 
 if(IlmBase_Found)
     link_libraries(IlmBase::Iex IlmBase::Half IlmBase::Imath IlmBase::IexMath)
