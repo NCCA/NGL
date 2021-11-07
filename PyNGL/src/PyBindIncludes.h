19b51547f7ed66e901c9540c12f28ae1c159d238
jmacey <jmacey@bournemouth.ac.uk>
2021-11-07 12:38:38 +0000

    forgot to commit new header file

---
+++ PyNGL/src/PyBindIncludes.h
@@ -0,0 +1,13 @@
+#ifndef PYBINDINCLUDES_H_
+#define PYBINDINCLUDES_H_
+#include <pybind11/pybind11.h>
+#include <pybind11/operators.h>
+#include <pybind11/stl.h>
+#include <pybind11/cast.h>
+#include <pybind11/stl_bind.h>
+#include <pybind11/pytypes.h>
+#include <pybind11/complex.h>
+
+
+
+#endif
